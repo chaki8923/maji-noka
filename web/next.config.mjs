53ba4977367d022f1ca2321f201ae857eb569fe8
茶木 涼 <>
2024-08-08 09:21:41 +0900

    vercel.json削除

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
     // X(Twitter)とGoogleのプロフィール画像を表示するために追加
     domains: ["pbs.twimg.com", "lh3.googleusercontent.com", "https://maji-image.s3.ap-northeast-1.amazonaws.com", "maji-image.s3.ap-northeast-1.amazonaws.com"],
   },
-  distDir: '.next'
 };
 
 export default nextConfig;
