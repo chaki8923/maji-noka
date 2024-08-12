a4791a6a83e3620ad6e44562e51d80798063bb0b
chaki <aowp.contact@gmail.com>
2024-08-12 12:52:49 +0900

    20240812-8

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: '.next',
   images: {
     // X(Twitter)とGoogleのプロフィール画像を表示するために追加
     domains: ["pbs.twimg.com", "lh3.googleusercontent.com", "https://maji-image.s3.ap-northeast-1.amazonaws.com", "maji-image.s3.ap-northeast-1.amazonaws.com"],
