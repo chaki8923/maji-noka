f00e1a724209a1d7910b954f5c8c1b239d09cb25
chaki <aowp.contact@gmail.com>
2024-08-13 13:47:49 +0900

    20240813-18

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  distDir: '.next',
-  images: {
-    // X(Twitter)とGoogleのプロフィール画像を表示するために追加
-    domains: ["pbs.twimg.com", "lh3.googleusercontent.com", "https://maji-image.s3.ap-northeast-1.amazonaws.com", "maji-image.s3.ap-northeast-1.amazonaws.com"],
+// next.config.mjs
+export default {
+  experimental: {
+    appDir: true,
   },
-};
-
-export default nextConfig;
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  // 他の設定があればここに追加
+}
