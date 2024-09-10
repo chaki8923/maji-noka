9bc74c1695921ea4347cc85bd4e445cd286070be
茶木 涼 <>
2024-09-10 14:53:26 +0900

    categiry削除

---
+++ web/src/server/routers/category.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import prisma from '../../../lib/prismadb'
+import { procedure, router } from "../trpc";
+
+// 新しいPrismaClientインスタンスを作成します。
+// const prisma = new PrismaClient();
+
+// 新しいrouterを作成します。
+export const categoryRouter = router({
+  
+
+  
+});
