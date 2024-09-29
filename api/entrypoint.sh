010f4a06ae4be58530936a16b54789c6699bd917
chaki8923 <aowp.contact@gmail.com>
2024-09-29 14:21:45 +0900

    emailとか設定

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -7,7 +7,7 @@ rm -f /app/tmp/pids/server.pid
 # production環境の場合のみ
 if [ "$RAILS_ENV" = "production" ]; then
   echo "===> Precompiling assets..."
-  bundle exec rails assets:precompile
+  # bundle exec rails assets:precompile
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
