97caa95ff8a7ba00169490c37939425a7b93847e
茶木 涼 <>
2024-10-03 09:02:25 +0900

    api デプロイ途中

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -6,8 +6,8 @@ rm -f /app/tmp/pids/server.pid
 
 # production環境の場合のみ
 if [ "$RAILS_ENV" = "production" ]; then
-  echo "===> Precompiling assets..."
-  bundle exec rails assets:precompile
+  echo "===> Precompiling assets...(しない)"
+  # bundle exec rails assets:precompile
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
